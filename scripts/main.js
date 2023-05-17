294f8eda8cd98f28d01914de5d072667bddaf5d6
m.ali petek <malipetek@gmail.com>
2023-05-17 10:01:45 +0300

    new script

--- scripts/main.js
+++ scripts/main.js
@@ -20,58 +20,63 @@ analytics.page();
 
 const visibleImages = [];
 
-function initImageObservation() {
-  const images = document.querySelectorAll('img');
-  const options = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.5
-  };
+const images = [...document.images];
+const options = {
+  root: null,
+  rootMargin: '0px',
+  threshold: 0.5
+};
 
-  const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        analytics.track('imageView', {
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      analytics.track('imageView', {
+        image: entry.target.src,
+        width: entry.target.width,
+        height: entry.target.height,
+        screen_width: screen.availWidth,
+        screen_height: screen.availHeight
+      });
+      visibleImages.push(entry.target.src);
+    } else {
+      const wasVisibleIndex = visibleImages.indexOf(entry.target.src);
+      if (wasVisibleIndex !== -1) {
+        visibleImages.splice(wasVisibleIndex, 1);
+        analytics.track('imageHide', {
           image: entry.target.src,
-          width: entry.target.width,
-          height: entry.target.height,
-          screen_width: screen.availWidth,
-          screen_height: screen.availHeight
         });
-        visibleImages.push(entry.target.src);
-      } else {
-        const wasVisibleIndex = visibleImages.indexOf(entry.target.src);
-        if (wasVisibleIndex !== -1) {
-          visibleImages.splice(wasVisibleIndex, 1);
-          analytics.track('imageHide', {
-            image: entry.target.src,
-          });
-        }
       }
-    });
-  }, options);
-
-  return {
-    images,
-    observer,
-    destroy: () => {
-      observer.disconnect();
     }
-  }
-}
+  });
+}, options);
 
-let observation = initImageObservation();
-
-observation.images.forEach(image => {
-  observation.observer.observe(image);
+images.forEach(image => {
+  observer.observe(image);
 });
 
 setInterval(() => {
-  if(visibleImages.length > 0) {
+  if (visibleImages.length > 0) {
     analytics.track('imageViewTick', {
       images: visibleImages,
     });
   }
-  observation.destroy();
-  observation = initImageObservation();
+
+  // check if document has new images start observing new ones and stop observing old ones
+  const newImages = [...document.images].filter(image => !images.includes(image));
+  newImages.forEach(image => {
+    observer.observe(image);
+  });
+  images.forEach(image => {
+    if (!newImages.includes(image)) {
+      observer.unobserve(image);
+    }
+  });
+  // modify images array
+  images.splice(0, images.length, ...newImages);
+  // also update visible images array
+  visibleImages.forEach(image => {
+    if (!newImages.includes(image)) {
+      visibleImages.splice(visibleImages.indexOf(image), 1);
+    }
+  });
 }, 10000);
